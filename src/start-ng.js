52170df72508346b3ee2f0afaf2fda2945d1e4b3
Wolfram Hempel <hempel.wolfram@gmail.com>
2015-06-03 15:42:40 +0200

    Updated structure

--- src/start-ng.js
+++ src/start-ng.js
@@ -1,80 +1,96 @@
-"use strict";
+/* global deepstream, angular */
+
 
 angular
 .module( 'ds-demo', [] )
 .service( 'deepstream', function() {
-	var client = deepstream( 'localhost:6020' );
-	client.login( {username: 'ds-simple-input-' + client.getUid()} );
-	return client;
-} )
-.controller( 'user', function( deepstream, $scope ) {
-	function User() {
-		this.record = deepstream.record.getAnonymousRecord();
-		this.getField = getField.bind( this, this.record );
-		this.firstname = this.getField( 'firstname' );
-		this.lastname = this.getField( 'lastname' );
-		this.title = this.getField( 'title' );
-		this.street = this.getField( 'street' );
-		this.number = this.getField( 'number' );
-		this.postcode = this.getField( 'postcode' );
-		this.city = this.getField( 'city' );
-	}
-	User.prototype.showUser = function( recordName ) {
-		this.record.setName( recordName );
+	return deepstream( 'localhost:6020' )
+		.login( {username: 'ng-example-app'} );
+})
+.service( 'bindFields', function(){
+	return function getField( $scope, record, names ) {
+		angular.forEach( names, function( name ){
+			Object.defineProperty( $scope, name, {
+				get: function() {
+					return record.get( name );
+				},
+				set: function( newValue ) {
+					if( newValue === undefined ) {
+						return;
+					}
+					record.set( name, newValue );
+				}
+			});
+		});
+		
+		record.subscribe(function() {
+			if( !$scope.$$phase ) {
+				$scope.$apply();
+			}
+		});
 	};
-	$scope.currentUser = new User();
+})
+.controller( 'user', function( deepstream, $scope, bindFields ) {
 
-	function Users() {
-		this.record = deepstream.record.getList( 'users' );
-		this.record.subscribe( this.updateEntries.bind( this ) );
-		this.list = [];
-	}
-	Users.prototype.updateEntries = function( entries ) {
+	var fields = [
+			'firstname',
+			'lastname',
+			'title',
+			'street',
+			'number',
+			'postcode',
+			'city'
+		];
+	var record = deepstream.record.getAnonymousRecord();
+	
+	bindFields( $scope, record, fields );
+
+	$scope.$root.$on( 'show-user', function( event, recordName ) {
+		$scope.name = recordName;
+		record.setName( recordName );
+	});
+})
+.controller( 'users', function( $scope, deepstream ){
+	var list = deepstream.record.getList( 'users' );
+	$scope.users = [];
+
+	list.subscribe(function( entries ){
 		function scopeApply() {
 			if( !$scope.$$phase ) {
 				$scope.$apply();
 			}
 		}
-		this.list = [];
-		for( var i = 0; i < entries.length; i++ ) {
-			var record = deepstream.record.getRecord( entries[i] );
+		$scope.users = entries.map(function( entry ){
+			var record = deepstream.record.getRecord( entry );
 			record.subscribe( scopeApply );
-			this.list.push( record );
-		}
-		scopeApply();
-	};
-	Users.prototype.addUser = function( ) {
-		var name = 'users/' + deepstream.getUid();
-		var record = deepstream.record.getRecord( name );
-		record.set( {
-			firstname: 'New',
-			lastname: 'User',
-			title: '-'
-		} );
-		this.record.addEntry( name );
-	};
-	Users.prototype.deleteUser = function( recordName ) {
-		this.record.removeEntry( recordName );
-	};
-	$scope.users = new Users();
+			return record;
+		});
 
-	function getField( record, name ) {
-		var self = this;
-		Object.defineProperty( self, name, {
-			get: function() {
-				return self.record.get( name );
-			},
-			set: function( newValue ) {
-				if( newValue === undefined ) {
-					return;
-				}
-				self.record.set( name, newValue );
-			}
-		} );
-		record.subscribe( name, function() {
-			if( !$scope.$$phase ) {
-				$scope.$apply();
-			}
-		} );
-	}
-} );
+		scopeApply();
+	});
+
+	$scope.addUser = function() {
+		var name = 'users/' + deepstream.getUid();
+		
+		deepstream
+			.record
+			.getRecord( name )
+			.set({
+				firstname: 'New',
+				lastname: 'User',
+				title: '-'
+			});
+
+		list.addEntry( name );
+	};
+
+	$scope.selectUser = function( recordName ) {
+		$scope.selectedUser = recordName;
+		$scope.$root.$emit( 'show-user', recordName );
+	};
+
+	$scope.deleteUser = function( recordName ) {
+		list.removeEntry( recordName );
+		deepstream.record.getRecord( recordName ).delete();
+	};
+});
